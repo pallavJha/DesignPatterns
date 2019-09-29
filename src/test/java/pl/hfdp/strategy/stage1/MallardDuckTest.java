deb49ef30b20ac01202572e2e63d3bf33ac3c648
Pallav Jha <pallav.jha@soroco.com>
2019-09-29 15:26:09 +0530

    configured test cases to run with maven

--- src/test/java/pl/hfdp/strategy/stage1/MallardDuckTest.java
+++ src/test/java/pl/hfdp/strategy/stage1/MallardDuckTest.java
@@ -4,7 +4,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static junit.framework.TestCase.assertEquals;
 
 public class MallardDuckTest {
 
--- src/test/java/pl/hfdp/strategy/stage1/RedHeadDuckTest.java
+++ src/test/java/pl/hfdp/strategy/stage1/RedHeadDuckTest.java
@@ -4,7 +4,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class RedHeadDuckTest {
 
